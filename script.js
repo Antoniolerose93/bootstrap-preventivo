0b8b29a03ac6128a7b848979fecbce2387407151
Antoniolerose93 <Antoniolerose.kr@gmail.com>
2025-06-05 18:38:40 +0200

    step 3

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log('preventivo')
 
 // recupero il form
 const formElement = document.getElementById('form-preventivo')
+// recupero i dati presenti all'interno del form
 const inputName = formElement.querySelector('#name')
 const inputSurname = formElement.querySelector('#surname')
 const inputEmail = formElement.querySelector('#email')
@@ -18,43 +19,45 @@ formElement.addEventListener('submit', function (event) {
 	console.log('submit del form', event)
 
 //leggo il valore inserito nella select lavoro
-let lavoroValue = lavoroSelezionato.value
-console.log(lavoroValue)
+	let lavoroValue = lavoroSelezionato.value
+	console.log(lavoroValue)
 
 //leggo il valore inserito nel codice sconto
-let scontoValue = codiceSconto.value
-console.log(scontoValue)
+	let scontoValue = codiceSconto.value
+	console.log(scontoValue)
 
 //imposto il prezzo orario, il prezzo base e e calcolo il prezzo finale dei lavori
-const oreRichieste = 10
+	const oreRichieste = 10
 
-let prezzoOrario;
+	let prezzoOrario;
 
-if (lavoroValue === 'backend' ) {
-	prezzoOrario = 20.50 
-} else if (lavoroValue === 'frontend'){
-	prezzoOrario = 15.30
-} else if (lavoroValue === 'analisi') {
-	prezzoOrario = 33.60
-}
+	if (lavoroValue === 'backend' ) {
+		prezzoOrario = 20.50 
+	} else if (lavoroValue === 'frontend'){
+		prezzoOrario = 15.30
+	} else if (lavoroValue === 'analisi') {
+		prezzoOrario = 33.60
+	} else if (lavoroValue ==='') {
+		prezzoOrario = 0
+	}	
 
-let prezzoBase = prezzoOrario * oreRichieste
-
-let prezzoFinale;
+	let prezzoBase = prezzoOrario * oreRichieste
+	
+	let prezzoFinale;
 
 //stabilisco gli sconti validi
-const codiceScontovalido = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24']
+	const codiceScontovalido = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24']
 
 
-if (codiceScontovalido.includes(scontoValue)) {
-    prezzoFinale = prezzoBase * 0.75;
-	prezzoOutputElement.innerHTML = `${prezzoFinale.toFixed(2)} &euro; - Sconto applicato 25%;` 
+	if (codiceScontovalido.includes(scontoValue)) {
+    	prezzoFinale = prezzoBase * 0.75;
+		prezzoOutputElement.innerHTML = `${prezzoFinale.toFixed(2)} &euro; - Sconto applicato 25%;` 
 	
-}  else {
-	prezzoFinale = prezzoBase
-	prezzoOutputElement.innerHTML = `${prezzoFinale.toFixed(2)} &euro; - Codice sconto non valido. Non hai diritto ad uno sconto;`
+	}  else {
+		prezzoFinale = prezzoBase
+		prezzoOutputElement.innerHTML = `${prezzoFinale.toFixed(2)} &euro; - Codice sconto non valido. Non hai diritto ad uno sconto;`
 		
-} 
+	} 
 
 })
 
